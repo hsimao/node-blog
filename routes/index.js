339e24344a9c880ccde421a46a3eacb5172aeb06
hsimao <e087754958@gmail.com>
2018-10-07 11:55:37 +0800

    add: paging logic, 新增分頁邏輯

--- routes/index.js
+++ routes/index.js
@@ -28,10 +28,50 @@ router.get("/", function(req, res) {
         }
       });
       articles.reverse(); // 文章資料排序反轉(最先創建文章在上方=>最後創建文章在上方)
+
+      // == 分頁邏輯開始
+      // 資料數量
+      const totalResult = articles.length;
+      // 每頁呈現數量
+      const perpage = 5;
+      // 總頁數
+      const pageTotal = Math.ceil(totalResult / perpage);
+      // 當前頁數
+      let currentPage = req.query.page || 1;
+      // 如果當前頁面小於1 或不是數字就=1
+      if (currentPage < 1 || !/^\d+$/.test(currentPage)) currentPage = 1;
+      if (currentPage > pageTotal) currentPage = pageTotal;
+
+      // 當下頁面需呈現資料位置換算
+      // 該頁資料起始位置
+      const minItem = currentPage * perpage - perpage + 1;
+      // 該頁資料結束位置
+      const maxItem = currentPage * perpage;
+      // console.log(
+      //   `總資料筆數:${totalResult}, 每頁呈現數量:${perpage}, 總頁數:${pageTotal}, 當前頁數:${currentPage}, 當前頁面第一筆:${minItem}, 當前頁面最後一筆${maxItem}`
+      // );
+
+      // 依據分頁條件印出資料
+      let newArticles = [];
+      articles.forEach((item, index) => {
+        let itemNum = index + 1;
+        if (itemNum >= minItem && itemNum <= maxItem) {
+          newArticles.push(item);
+        }
+      });
+      const page = {
+        pageTotal,
+        currentPage,
+        prev: currentPage > 1,
+        next: currentPage < pageTotal
+      };
+      // == 分頁邏輯結束
+
       res.render("index", {
         title: "Express",
-        articles,
+        articles: newArticles,
         categories,
+        page,
         stringtags,
         moment
       });
